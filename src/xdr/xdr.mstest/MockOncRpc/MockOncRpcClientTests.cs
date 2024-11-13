8713a64aa6f7d0a41b775ac10690bcf8645c8a85
David Hary <791370+ATECoder@users.noreply.github.com>
2024-11-13 12:26:39 -0800

    * Note: Fixes are required with upgrade of Forms libraries and Test and Demo apps to .Net 9.0. * Update change log. * Update packages. * Increment version to 9083.

--- src/xdr/xdr.mstest/MockOncRpc/MockOncRpcClientTests.cs
+++ src/xdr/xdr.mstest/MockOncRpc/MockOncRpcClientTests.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using System.Text;
 using cc.isr.XDR.MSTest.Codecs;
-using cc.isr.MSTest.Exceptions;
+using cc.isr.Std.MSTest.Exceptions;
 
 namespace cc.isr.XDR.MSTest.MockOncRpc;
 
@@ -63,6 +63,7 @@ public class MockOncRpcClientTests
     [TestInitialize()]
     public void InitializeBeforeEachTest()
     {
+        Console.WriteLine( $"{this.TestContext?.FullyQualifiedTestClassName}: {DateTime.Now} {System.TimeZoneInfo.Local}" );
         if ( Logger is not null )
         {
             this._loggerScope = Logger.BeginScope( this.TestContext?.TestName ?? string.Empty );
--- src/xdr/xdr.mstest/Usings.cs
+++ src/xdr/xdr.mstest/Usings.cs
@@ -1,4 +1,4 @@
 global using Microsoft.Extensions.Logging;
-global using cc.isr.MSTest;
+global using cc.isr.Std.Tests;
 global using cc.isr.XDR.Codecs;
 global using cc.isr.XDR.EncodingExtensions;
