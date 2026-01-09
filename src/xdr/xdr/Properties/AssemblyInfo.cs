3a48dab21c758ce76ddc1338c4dff721b8d2d885
David Hary <791370+ATECoder@users.noreply.github.com>
2026-01-09 10:12:29 -0800

    - Simplify null check. - Remove empty top lines.

--- src/xdr/xdr/Properties/AssemblyInfo.cs
+++ src/xdr/xdr/Properties/AssemblyInfo.cs
@@ -1,5 +1,2 @@
 
-// in source file:
-// [assembly: System.CLSCompliant( false )]
-// [assembly: System.Runtime.InteropServices.ComVisible( false )]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "cc.isr.Xdr.MSTest,PublicKey=" + cc.isr.XDR.SolutionInfo.PublicKey )]
